dbbe0f64c06f5a14ec7d4bd48b61a3f0ce61bd81
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-12-27 12:44:14 +0000

    pre script

--- input/global/event.js
+++ input/global/event.js
@@ -1,4 +1,22 @@
 /**
-an event defined in a doclet
-@event docletEvent
+@class
 */
+function Hurl () {}
+
+/**
+ * Throw a snowball.
+ *
+ * @fires Hurl#snowball
+ */
+Hurl.prototype.snowball = function () {
+  /**
+   * Snowball event.
+   *
+   * @event Hurl#snowball
+   * @type {object}
+   * @property {boolean} isPacked - Indicates whether the snowball is tightly packed.
+   */
+  this.emit('snowball', {
+    isPacked: this._snowball.isPacked
+  })
+}
