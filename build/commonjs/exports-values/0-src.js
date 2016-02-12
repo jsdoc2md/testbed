e2ef7ebfea4e2894a307bf33ae10061ad8527b81
Lloyd Brookes <lloyd.brookes@gmail.com>
2016-02-12 15:38:48 +0000

    exports-vars/values

---
+++ build/commonjs/exports-values/0-src.js
@@ -0,0 +1,13 @@
+/**
+ * note the exports are documented directly
+ * @module exports-values
+ */
+
+/**
+* @type {number}
+*/
+exports.one = 1
+/**
+ * @type {number}
+ */
+exports.two = 2
---
+++ build/commonjs/exports-vars/0-src.js
@@ -0,0 +1,17 @@
+/**
+ * note the vars are documented, not the exports. 
+ * @module exports-vars
+ */
+
+/**
+* @type {number}
+*/
+var one = 1
+
+/**
+ * @type {number}
+ */
+var two = 2
+
+exports.one = one
+exports.two = two
