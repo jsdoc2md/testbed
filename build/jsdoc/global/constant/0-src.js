cdcfd07974dac00b5df4d79d316e6bec6a3c213c
Lloyd Brookes <lloyd.brookes@gmail.com>
2016-02-24 12:07:19 +0000

    constant

---
+++ build/jsdoc/global/constant/0-src.js
@@ -0,0 +1,13 @@
+/**
+ * the first important constant
+ * @constant {number}
+ */
+var CONST_ONE = 1
+
+/**
+ * include `@type` and `@default`
+ * @constant
+ * @type {number}
+ * @default
+ */
+var CONST_TWO = 2
