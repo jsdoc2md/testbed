7fd47abd692f5ef268e50666460a26383251eed3
Lloyd Brookes <lloyd.brookes@gmail.com>
2018-08-07 19:09:58 +0100

    issue-158

---
+++ buggy/issue-158.js
@@ -0,0 +1,15 @@
+/**
+ * Gets an item from storage.
+ * @async
+ * @param {object} itemInfo - the details about the item
+ * @return {object} the retrieved item
+ */
+async function getItem(itemInfo) {}
+
+/**
+ * Gets a category from storage.
+ * @async
+ * @param {object} categoryInfo - the details about the category
+ * @returns {object} the retrieved category
+ */
+async function getCategory(categoryInfo) {}
