bff044fa4e072fb5df4d791513abf2c451715600
Lloyd Brookes <lloyd.brookes@gmail.com>
2024-09-01 13:39:17 +0100

    update

--- build/jsdoc/commonjs/module-exports-func-no-module-tag/0-src.js
+++ build/jsdoc/commonjs/module-exports-func-no-module-tag/0-src.js
@@ -1,4 +1,4 @@
-/* This file produces no output, creating the most common confusion users report. */
+/* This file produces no output, creating the most common confusion users report. Should cause dmd to print a warning. */
 
 /**
  * Add two values.
