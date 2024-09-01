9b16556f79b78511e9dd53a9b520fe9f9c82f95b
Lloyd Brookes <lloyd.brookes@gmail.com>
2024-09-01 13:05:25 +0100

    added module-exports-func-no-module-tag

---
+++ build/jsdoc/commonjs/module-exports-func-no-module-tag/0-src.js
@@ -0,0 +1,8 @@
+/* This file produces no output, creating the most common confusion users report. */
+
+/**
+ * @param {number}
+ * @param {number}
+ * @returns {number}
+ */
+module.exports = function (one, two) {}
